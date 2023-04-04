44ab4791b968eb6cdc548180dbfdd2f16b337e0e
GioF_71 <giovanni.fulco@gmail.com>
2023-04-04 10:33:30 +0200

    progress

--- xfce-vnc/service-install.sh
+++ xfce-vnc/service-install.sh
@@ -4,6 +4,10 @@ set -ex
 
 SERVICE_FILE_NAME=vncserver.service
 SYSTEMD_SERVICE="$HOME/.config/systemd/user/${SERVICE_FILE_NAME}"
+
+echo "SERVICE_FILE_NAME=[$SERVICE_FILE_NAME]"
+echo "SYSTEMD_SERVICE=[$SYSTEMD_SERVICE]"
+
 if [ -f "${SERVICE_FILE_NAME}" ]; then
     if [ -f "${SYSTEMD_SERVICE}" ]; then
         rm "${SYSTEMD_SERVICE}"
