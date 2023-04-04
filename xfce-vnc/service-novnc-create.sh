a33dd0e4f4c2d8b912b40a77a99d58a27a365ed7
GioF_71 <giovanni.fulco@gmail.com>
2023-04-04 12:15:47 +0200

    service create for novnc draft

---
+++ xfce-vnc/service-novnc-create.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ../util/load_config.sh
+
+load_config .config
+
+CERT_DIR=$HOME/certificate
+if [[ -d "$CERT_DIR" ]]; then
+    echo "Certificate directory [$CERT_DIR] already exists."
+else
+    echo "Home directory [$CERT_DIR] does not exist, will be created"
+    mkdir -p $CERT_DIR
+fi
+
+CERT_FILE="${CERT_DIR}/novnc.pem"
+
+if [[ -f "$CERT_FILE"]]; then
+    echo "Certificate file [${CERT_FILE}] already exists."
+else
+    echo "Certificate file [${CERT_FILE}] does not exist, creating ..."
+    openssl req -x509 -nodes -newkey rsa:3072 \
+        -keyout $CERT_FILE \
+        -out $CERT_FILE \
+        -days 3650 \
+        -subj '/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com'
+    echo "Certificate file [${CERT_FILE}] created."
+fi
