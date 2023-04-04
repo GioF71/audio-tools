2de44708b4630282fcab8e7295ac128642684143
Giovanni Fulco <giovanni.fulco@gmail.com>
2023-04-04 10:43:13 +0200

    progress

--- xfce-vnc/service-install.sh
+++ xfce-vnc/service-install.sh
@@ -3,19 +3,21 @@
 set -ex
 
 SERVICE_FILE_NAME=vncserver.service
-SYSTEMD_SERVICE="$HOME/.config/systemd/user/${SERVICE_FILE_NAME}"
+SYSTEMD_PATH="$HOME/.config/systemd/user"
+SYSTEMD_SERVICE="${SYSTEMD_PATH}/${SERVICE_FILE_NAME}"
 
 echo "SERVICE_FILE_NAME=[$SERVICE_FILE_NAME]"
 echo "SYSTEMD_SERVICE=[$SYSTEMD_SERVICE]"
 
 if [ -f "${SERVICE_FILE_NAME}" ]; then
-    if [ -f "${SYSTEMD_SERVICE}" ]; then
-        rm "${SYSTEMD_SERVICE}"
-        ln "$SERVICE_FILE_NAME" "${SYSTEMD_SERVICE}"
-        echo "Reloading systemd ..."
-        systemctl --user daemon-reload
-        echo "Systemd reloaded."
-    fi
+        if [ -f "${SYSTEMD_SERVICE}" ]; then 
+		rm "${SYSTEMD_SERVICE}"
+	fi
+	mkdir -p "${SYSTEMD_PATH}"	
+	ln "$SERVICE_FILE_NAME" "${SYSTEMD_SERVICE}"
+	echo "Reloading systemd ..."
+	systemctl --user daemon-reload
+	echo "Systemd reloaded."
 else
-    echo "Service file ${SERVICE_FILE_NAME} missing, create with service-create.sh"
+	echo "Service file ${SERVICE_FILE_NAME} missing, create with service-create.sh"
 fi
